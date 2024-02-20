7994ce252a900a1c8f6a618c72161cd8a0beb67d
prtkjakhar <prtkjakhar@gmail.com>
2024-02-20 17:23:53 +0530

    chore: modified login molecule using mui

--- molecules/LoginPage/index.tsx
+++ molecules/LoginPage/index.tsx
@@ -1,47 +1,86 @@
 import React, { useState } from 'react';
 import styles from './index.module.css';
-import Image from 'next/image';
-import loginImg from './login.png';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 interface LoginPageProps {
-  handleOTPPage: (prop: string) => void;
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
 }
 
-const LoginPage: React.FC<LoginPageProps> = ({
-  handleOTPPage,
-}) => {
-  const [input, setInput] = useState("");
-  const handleNumber: React.ChangeEventHandler = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setInput(e.target.value);
+const LoginPage: React.FC<LoginPageProps> = ({ handleSubmit }) => {
+  const [isAadharClicked, setIsAadharClicked] = useState(false);
+
+  const handleAadharClick = () => {
+    setIsAadharClicked((prop) => !prop);
   };
+
   return (
     <>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
-      <div className={`${styles.main}`}>
-        <div className={styles.subtitle}>Bot</div>
-        <div className={styles.body}>
-          <Image src={loginImg} alt="" height={201} width={300} />
-          <form onSubmit={(event) => event?.preventDefault()}>
-            <div className={styles.container}>
-              <div className={styles.inputContainer}>
-                <input
-                  type="text"
-                  pattern="[0-9]*"
-                  placeholder="Enter Mobile Number"
-                  value={input}
-                  onChange={handleNumber}
-                />
-              </div>
-              <button className={styles.submitButton} onClick={() => handleOTPPage(input)}>
-                Continue
-              </button>
-            </div>
-          </form>
-        </div>
+      <div className={styles.main}>
+        <Typography
+          component="h1"
+          variant="h4"
+          textAlign="left"
+          width="90%"
+          color="#1E232C">
+          Welcome!
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          noValidate
+          sx={{ mt: 1, width: '90%' }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label={
+              isAadharClicked ? 'Enter Aadhar Number' : 'Enter Phone Number'
+            }
+            name={
+              isAadharClicked ? 'aadhar' : 'phone'
+            }
+            autoComplete={isAadharClicked ? 'aadhar' : 'phone'}
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 4,
+              p: 1,
+              background: '#1E232C',
+              borderRadius: '10px',
+            }}>
+            Login
+          </Button>
+        </Box>
+        <Typography
+          variant="caption"
+          textAlign="center"
+          width="90%"
+          color="#1E232C"
+          sx={{ mb: 1 }}>
+          or Login using
+        </Typography>
+        <Button fullWidth variant="text" onClick={handleAadharClick}>
+          <Typography
+            variant="button"
+            sx={{
+              textDecoration: 'underline',
+              color: '#1E232C',
+              fontWeight: 'bold',
+            }}>
+            {!isAadharClicked ? 'Aadhar Number' : 'Phone Number'}
+          </Typography>
+        </Button>
       </div>
     </>
   );
