e9b1cba77dbafa10ae7ebf2ea4b3a90a13aede25
prtkjakhar <prtkjakhar@gmail.com>
2024-03-04 16:46:01 +0530

    added updated login molecule

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Navbar } from "./components/navbar";
 import { Route, Routes } from "react-router-dom";
-import LoginPage from "./molecules/LoginPage";
+import LoginPage from "./molecules/LoginMobileAadharPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
+    <Toaster/>
       <Navbar />
       <>
         <Routes>
-          <Route index element={<LoginPage handleOTPPage={() => null} />} />
+          <Route index element={<LoginPage />} />
           <Route
             path="/LoginPage"
-            element={<LoginPage handleOTPPage={() => null} />}
+            element={<LoginPage />}
           />
         </Routes>
       </>
---
+++ src/molecules/LoginMobileAadharPage/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy', // For handling CSS modules
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js', // For handling static assets
+  },
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
