257a182a2d786a1231d9b974646d60e43ea3cee0
riya-2206 <riya@esmagico.in>
2024-03-11 16:06:03 +0530

    made changes in test cases by using config file

--- src/molecules/ComingSoonPage/index.test.tsx
+++ src/molecules/ComingSoonPage/index.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen} from '@testing-library/react';
 import ComingSoonPage from './index';
+import config from './config.json';
 
 describe('Coming Soon component', () => {
   test('renders without crashing', () => {
@@ -8,20 +9,31 @@ describe('Coming Soon component', () => {
 
   test('displays "Coming Soon Title" text', () => {
     const { getByText } = render(<ComingSoonPage />);
-    const textElement = getByText(/Coming Soon!/i);
+    const textElement = getByText(config.component.title);
     expect(textElement).toBeInTheDocument();
   });
 
   test('displays "Description" text', () => {
     const { getByText } = render(<ComingSoonPage />);
-    const textElement = getByText(/We are going to launch this feature very soon. Stay tuned!/i);
+    const textElement = getByText(config.component.description);
     expect(textElement).toBeInTheDocument();
   });
 
   test('renders button correctly', () => {
     render(<ComingSoonPage />);
-    const buttonElement = screen.getByText('Back');
+    const buttonElement = screen.getByText(config.component.backText);
     expect(buttonElement).toBeInTheDocument();
   });
+
+  test('renders coming soon image with correct src and alt text', () => {
+  const src = config.component.comingSoonImage;
+  const alt = 'hourGlass';
+
+  const { getByAltText } = render(<ComingSoonPage />);
+
+  const imageElement = getByAltText(alt);
+  expect(imageElement).toBeInTheDocument();
+  expect(imageElement).toHaveAttribute('src', src);
+});
 
 });
--- src/molecules/ComingSoonPage/index.tsx
+++ src/molecules/ComingSoonPage/index.tsx
@@ -3,13 +3,11 @@ import styles from './index.module.css';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import hourGlassIcon from "./assets/hourglass.svg"
 import config from './config.json';
 
 const ComingSoonPage: React.FC = () => {
   const handleBack = ()=>{
-    // window?.history?.back()
-    console.log(config.component.backText)
+    window?.history?.back()
   }
 
   return (
@@ -18,10 +16,10 @@ const ComingSoonPage: React.FC = () => {
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
         <Box my={15} className={styles.container}>
-          <Box mt={5}><Typography variant='h4' sx={{color: config.theme.primaryColor.value, fontWeight: "700"}}>{config.component.title?config.component.title:"Coming Soon!"}</Typography></Box>
-          <Box><img src={config.component.comingSoonImage? config.component.comingSoonImage: hourGlassIcon} alt="hourGlass" className={styles.imageContainer}/></Box>
-          <Box><Typography variant='body1' sx={{fontWeight:"600", color: config.theme.secondaryColor.value}}>{config.component.description?config.component.description:"We are going to launch this feature very soon. Stay tuned!"}</Typography></Box>
-          <Box my={5}><Button variant='contained' className={styles.backButton} size='large' style={{backgroundColor: config.theme.primaryColor.value}} onClick={handleBack}>{config.component.backText?config.component.backText:"Back"}</Button></Box>
+          <Box mt={5}><Typography variant='h4' sx={{color: config.theme.primaryColor.value, fontWeight: "700"}}>{config.component.title}</Typography></Box>
+          <Box><img src={config.component.comingSoonImage} alt="hourGlass" className={styles.imageContainer}/></Box>
+          <Box><Typography variant='body1' sx={{fontWeight:"600", color: config.theme.secondaryColor.value}}>{config.component.description}</Typography></Box>
+          <Box my={5}><Button variant='contained' className={styles.backButton} size='large' style={{backgroundColor: config.theme.primaryColor.value}} onClick={handleBack}>{config.component.backText}</Button></Box>
         </Box>
     </>
   );
