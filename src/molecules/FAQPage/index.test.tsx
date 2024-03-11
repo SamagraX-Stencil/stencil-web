88aafa8adbf48df3b4bee075b6111491b344a25b
riya-2206 <riya@esmagico.in>
2024-03-11 16:30:26 +0530

    made changes in test cases by using config file

--- src/molecules/FAQPage/index.test.tsx
+++ src/molecules/FAQPage/index.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import FAQPage from './index';
+import config from './config.json';
 
 
 describe('FAQ component', () => {
@@ -10,25 +11,37 @@ describe('FAQ component', () => {
 
   test('displays "FAQ Title" text', () => {
     const { getByText } = render(<FAQPage />);
-    const textElement = getByText(/FAQs/i);
+    const textElement = getByText(config.component.title);
     expect(textElement).toBeInTheDocument();
   });
 
   test('displays "Contact description" text', () => {
     const { getByText } = render(<FAQPage />);
-    const textElement = getByText(/To connect with call centre/i);
+    const textElement = getByText(config.component.contactDescriptionText);
     expect(textElement).toBeInTheDocument();
   });
 
   test('renders contact button correctly', () => {
     render(<FAQPage />);
-    const buttonElement = screen.getByText('Dial 155333');
+    const buttonElement = screen.getByText(config.component.contactText);
     expect(buttonElement).toBeInTheDocument();
   });
 
   test('renders Manual User button correctly', () => {
     render(<FAQPage />);
-    const buttonElement = screen.getByText('User Manual - For VAWs');
+    const buttonElement = screen.getByText(config.component.userManualText);
     expect(buttonElement).toBeInTheDocument();
   });
+
+  test('renders downtime image with correct src and alt text', () => {
+  const src = config.component.contactIcon;
+  const alt = 'callIcon';
+
+  const { getByAltText } = render(<FAQPage />);
+
+  const imageElement = getByAltText(alt);
+  expect(imageElement).toBeInTheDocument();
+  expect(imageElement).toHaveAttribute('src', src);
+});
+
 });
--- src/molecules/FAQPage/index.tsx
+++ src/molecules/FAQPage/index.tsx
@@ -3,12 +3,15 @@ import styles from './index.module.css';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import callIcon from './assets/call-icon.svg'
 import config from './config.json';
 
 const FAQPage: React.FC = () => {
   const downloadPDFHandler=()=>{
     console.log(config.component.userManualText)
+  }
+
+  const handleContactClick=()=>{
+    console.log(config.component.contactText)
   }
 
   return (
@@ -17,24 +20,24 @@ const FAQPage: React.FC = () => {
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
       <Box className={styles.main}>
-          <Box m={3}><Typography variant='h4' sx={{fontWeight:"600", color: config.theme.primaryColor.value}}>{config.component.title?config.component.title:"FAQs"}</Typography></Box>
+          <Box m={3}><Typography variant='h4' sx={{fontWeight:"600", color: config.theme.primaryColor.value}}>{config.component.title}</Typography></Box>
           <Box>
             {config?.component?.userManualText && (
               <Box className={styles.manualButtons} m={3}>
                 <Button
                   onClick={downloadPDFHandler}
                   variant='contained' sx={{textTransform:'none', backgroundColor:config.theme.primaryColor.value, "&:hover":{backgroundColor:config.theme.primaryColor.value}}}>
-                  {config.component.userManualText?config.component.userManualText:"User Manual - For VAWs"}
+                  {config.component.userManualText}
                 </Button>
               </Box>
             )}
             {config?.component?.contactText && (
               <Box className={styles.dialerBox} m={3}>
-                <Box p={1.5}><Typography variant='body1' sx={{fontWeight:"bold"}}>{config.component.contactDescriptionText?config.component.contactDescriptionText:"To connect with call centre"}</Typography>
+                <Box p={1.5}><Typography variant='body1' sx={{fontWeight:"bold"}}>{config.component.contactDescriptionText}</Typography>
                 </Box>
                 <Box px={2} >
-                  <img src={callIcon?callIcon:config.component.contactIcon} alt="callIcon" className={styles.callIconBox}/>
-                  <Button variant='text' size="large" sx={{textTransform:'none',color:config.theme.primaryColor.value, "&:hover":{color:config.theme.primaryColor.value}}}><Typography variant='h5' fontWeight={600}>{config.component.contactText?config.component.contactText:"Dial 155333"}</Typography></Button>
+                  <img src={config.component.contactIcon} alt="callIcon" className={styles.callIconBox}/>
+                  <Button variant='text' size="large" onClick={handleContactClick} sx={{textTransform:'none',color:config.theme.primaryColor.value, "&:hover":{color:config.theme.primaryColor.value}}}><Typography variant='h5' fontWeight={600}>{config.component.contactText}</Typography></Button>
                 </Box>
               </Box>
             )}
