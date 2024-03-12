608ad4af1ff355f9f63a7235f0faa75ffeed6315
riya-2206 <riya@esmagico.in>
2024-03-13 00:35:51 +0530

    provided default object for theme context

--- src/molecules/theme-provider/theme-context.ts
+++ src/molecules/theme-provider/theme-context.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-import { Theme } from "@mui/material/styles";
+import { Theme, createTheme } from "@mui/material/styles";
 import { Color } from "../../components/theme-picker";
 
 interface ThemeContextType {
@@ -9,6 +9,10 @@ interface ThemeContextType {
 }
 
 export const ThemeContext = createContext<ThemeContextType | undefined>(
-  undefined
+  {
+    theme: createTheme(), // Replace with your default theme creation function
+    modifyTheme: () => {}, // Empty function for now
+    modifyPaletes: () => {} // Empty function for now
+  }
 );
 
