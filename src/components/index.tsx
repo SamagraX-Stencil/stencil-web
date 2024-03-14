85452e312c0c957d04a507a779eccd16bf9fc5c1
Shryansh <shryansh2024@gmail.com>
2024-03-14 19:33:22 +0530

    added voice recorder molecule

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,9 @@
 import { Box, Container } from "@mui/material";
 import { OTPInput } from "../molecules/OTPInput";
-
+import VoiceRecorder from '../molecules/VoiceRecorder'
+import { useState } from 'react'
 const Components = () => {
+  const [inputMsg, setInputMsg] = useState('')
   return (
     <Box
       minHeight="95vh" // Full viewport height
@@ -12,6 +14,13 @@ const Components = () => {
         <h4>OTP Input</h4>
         <div className="mt-2 p-5 border">
           <OTPInput separator="-" length={4} value="" onChange={() => null} />
+        </div>
+        <div className='mt-2 p-5 border'>
+          <VoiceRecorder
+            setInputMsg={setInputMsg}
+            tapToSpeak={false}
+            includeDiv={false}
+          />
         </div>
       </Container>
     </Box>
---
+++ src/molecules/VoiceRecorder/index.tsx
@@ -0,0 +1,303 @@
+import { useState } from 'react'
+import styles from './styles.module.css'
+import toast from 'react-hot-toast'
+import config from './config.json'
+
+interface VoiceRecorder {
+  setInputMsg: (msg: string) => void
+  tapToSpeak: boolean
+  includeDiv?: boolean
+}
+
+const VoiceRecorder: React.FC<VoiceRecorder> = ({
+  setInputMsg,
+  tapToSpeak,
+  includeDiv = false,
+}) => {
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null)
+  const [userClickedError, setUserClickedError] = useState(false)
+  const [apiCallStatus, setApiCallStatus] = useState('idle')
+
+  const VOICE_MIN_DECIBELS: number = -35
+  const delayBetweenDialogs: number = config.component.delayBetweenDialogs
+  const DIALOG_MAX_LENGTH: number = 60 * 1000
+  let IS_RECORDING: boolean = false
+
+  const startRecording = async () => {
+    IS_RECORDING = true
+    record()
+  }
+
+  const stopRecording = () => {
+    IS_RECORDING = false
+    if (mediaRecorder !== null) {
+      mediaRecorder.stop()
+      setMediaRecorder(null)
+    }
+  }
+
+  function record() {
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      //start recording:
+      const recorder = new MediaRecorder(stream)
+      recorder.start()
+      setMediaRecorder(recorder)
+
+      //save audio chunks:
+      const audioChunks: BlobPart[] = []
+      recorder.addEventListener('dataavailable', (event) => {
+        audioChunks.push(event.data)
+      })
+
+      //analysis:
+      const audioContext = new AudioContext()
+      const audioStreamSource = audioContext.createMediaStreamSource(stream)
+      const analyser = audioContext.createAnalyser()
+      analyser.minDecibels = VOICE_MIN_DECIBELS
+      audioStreamSource.connect(analyser)
+      const bufferLength = analyser.frequencyBinCount
+      const domainData = new Uint8Array(bufferLength)
+
+      //loop:
+      let time: Date = new Date()
+      let startTime: number
+      let lastDetectedTime: number = time.getTime()
+      let anySoundDetected: boolean = false
+      const detectSound = () => {
+        //recording stopped by user:
+        if (!IS_RECORDING) return
+
+        time = new Date()
+        const currentTime = time.getTime()
+
+        //time out:
+        if (currentTime > startTime + DIALOG_MAX_LENGTH) {
+          recorder.stop()
+          return
+        }
+
+        //a dialog detected:
+        if (
+          anySoundDetected === true &&
+          currentTime > lastDetectedTime + delayBetweenDialogs
+        ) {
+          recorder.stop()
+          return
+        }
+
+        //check for detection:
+        analyser.getByteFrequencyData(domainData)
+        for (let i = 0; i < bufferLength; i++)
+          if (domainData[i] > 0) {
+            anySoundDetected = true
+            time = new Date()
+            lastDetectedTime = time.getTime()
+          }
+
+        //continue the loop:
+        window.requestAnimationFrame(detectSound)
+      }
+      window.requestAnimationFrame(detectSound)
+
+      //stop event:
+      recorder.addEventListener('stop', () => {
+        //stop all the tracks:
+        stream.getTracks().forEach((track) => track.stop())
+        if (!anySoundDetected) return
+
+        //send to server:
+        const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' })
+        makeComputeAPICall(audioBlob)
+      })
+    })
+  }
+  const makeComputeAPICall = async (blob: Blob) => {
+    try {
+      setApiCallStatus('processing')
+      console.log('base', blob)
+      toast.success(`${config.component.messageRecorderWait}`)
+
+      // Define the API endpoint
+      const apiEndpoint = process.env.REACT_APP_BASE_URL
+
+      // Create a FormData object
+      const formData = new FormData()
+      // Append the WAV file to the FormData object
+      formData.append('file', blob, 'audio.wav')
+      formData.append('phoneNumber', localStorage.getItem('phoneNumber') || '')
+
+      // Send the WAV data to the API
+      const resp = await fetch(apiEndpoint + '/aitools/asr', {
+        method: 'POST',
+        body: formData,
+      })
+
+      if (resp.ok) {
+        const rsp_data = await resp.json()
+        console.log('hi', rsp_data)
+        if (rsp_data.text === '')
+          throw new Error('Unexpected end of JSON input')
+        setInputMsg(rsp_data.text)
+        sessionStorage.setItem('asrId', rsp_data.id)
+      } else {
+        toast.error(`${config.component.recorderErrorMessage}`)
+        console.log(resp)
+        // Set userClickedError to true when an error occurs
+        setUserClickedError(false)
+
+        // Automatically change back to startIcon after 3 seconds
+        setTimeout(() => {
+          // Check if the user has not clicked the error icon again
+          if (!userClickedError) {
+            setApiCallStatus('idle')
+          }
+        }, 2500)
+      }
+      setApiCallStatus('idle')
+    } catch (error) {
+      console.error(error)
+      setApiCallStatus('error')
+      toast.error(`${config.component.recorderErrorMessage}`)
+      // Set userClickedError to true when an error occurs
+      setUserClickedError(false)
+
+      // Automatically change back to startIcon after 3 seconds
+      setTimeout(() => {
+        // Check if the user has not clicked the error icon again
+        if (!userClickedError) {
+          setApiCallStatus('idle')
+        }
+      }, 2500)
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        {mediaRecorder && mediaRecorder.state === 'recording' ? (
+          <div className={styles.center}>
+            {includeDiv ? (
+              <div className={styles.imgContainer}>
+                <img
+                  src={config.component.stopIcon}
+                  alt='stopIcon'
+                  onClick={() => {
+                    stopRecording()
+                  }}
+                  style={{ cursor: 'pointer', height: '40px', width: '40px' }}
+                />
+              </div>
+            ) : (
+              <img
+                src={config.component.stopIcon}
+                alt='stopIcon'
+                onClick={() => {
+                  stopRecording()
+                }}
+                style={{ cursor: 'pointer', height: '40px', width: '40px' }}
+              />
+            )}
+          </div>
+        ) : (
+          <div className={styles.center}>
+            {apiCallStatus === 'processing' ? (
+              includeDiv ? (
+                <div className={styles.imgContainer}>
+                  <img
+                    src={config.component.processingIcon}
+                    alt='processingIcon'
+                    style={{ cursor: 'pointer', height: '40px', width: '40px' }}
+                  />
+                </div>
+              ) : (
+                <img
+                  src={config.component.processingIcon}
+                  alt='processingIcon'
+                  style={{ cursor: 'pointer', height: '40px', width: '40px' }}
+                />
+              )
+            ) : apiCallStatus === 'error' ? (
+              includeDiv ? (
+                <div className={styles.imgContainer}>
+                  <img
+                    src={config.component.errorIcon}
+                    alt='errorIcon'
+                    onClick={() => {
+                      setUserClickedError(true)
+                      startRecording()
+                    }}
+                    style={{ cursor: 'pointer', height: '40px', width: '40px' }}
+                  />
+                </div>
+              ) : (
+                <img
+                  src={config.component.errorIcon}
+                  alt='errorIcon'
+                  onClick={() => {
+                    setUserClickedError(true)
+                    startRecording()
+                  }}
+                  style={{ cursor: 'pointer', height: '40px', width: '40px' }}
+                />
+              )
+            ) : (
+              <>
+                {includeDiv ? (
+                  <div className={styles.imgContainer}>
+                    <img
+                      src={config.component.startIcon}
+                      alt='startIcon'
+                      onClick={() => {
+                        setUserClickedError(true)
+                        startRecording()
+                      }}
+                      style={{
+                        cursor: 'pointer',
+                        height: '40px',
+                        width: '40px',
+                      }}
+                    />
+                  </div>
+                ) : (
+                  <img
+                    // src={context?.kaliaClicked ? disabledStart : start}
+                    src={config.component.startIcon}
+                    alt='startIcon'
+                    onClick={() => {
+                      // if (context?.kaliaClicked) return
+                      setUserClickedError(true)
+                      startRecording()
+                    }}
+                    // style={{
+                    //   cursor: context?.kaliaClicked ? 'not-allowed' : 'pointer',
+                    // }}
+                    style={{
+                      cursor: 'pointer',
+                      height: '40px',
+                      width: '40px',
+                    }}
+                  />
+                )}
+                {tapToSpeak ? (
+                  <p
+                    style={{
+                      color: 'black',
+                      fontSize: '12px',
+                      marginTop: '4px',
+                    }}
+                  >
+                    {'label.tap_to_speak'}
+                  </p>
+                ) : (
+                  <></>
+                )}
+              </>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default VoiceRecorder
