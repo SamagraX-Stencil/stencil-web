f883910dfe6c5285708bbda3b0737f5489a085bd
biratdatta <birat.datta26@gmail.com>
2024-03-16 22:17:27 +0530

    fixed vercel deployed issues

--- src/molecules/sidebar/index.tsx
+++ src/molecules/sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
 import List from "@mui/material/List";
