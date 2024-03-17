8fb09b15ab597388e4172e2d1f207b8673b2580e
biratdatta <birat.datta26@gmail.com>
2024-03-17 23:43:30 +0530

    fixed dev issues

--- src/molecules/theme-provider/theme-context.ts
+++ src/molecules/theme-provider/theme-context.ts
@@ -2,7 +2,6 @@ import { createContext } from "react";
  
 import { Theme } from "@mui/material/styles";
  
-import { Theme, createTheme } from "@mui/material/styles";
  
 import { Color } from "../../components/theme-picker";
 
@@ -16,12 +15,7 @@ export const ThemeContext = createContext<ThemeContextType | undefined>(
  
   undefined
  
-{
-
-    theme: createTheme(), // Replace with your default theme creation function
-    modifyTheme: () => {}, // Empty function for now
-    modifyPaletes: () => {} // Empty function for now
-  }
+ 
  
 );
 
