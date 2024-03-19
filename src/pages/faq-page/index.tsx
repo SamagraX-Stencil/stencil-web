8984c3c216a273837946f20a4e33efd32b84cc5f
riya-2206 <riya@esmagico.in>
2024-03-19 10:58:49 +0530

    theme implementation and some changes done in test cases for faq page

--- src/pages/faq-page/index.tsx
+++ src/pages/faq-page/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styles from './index.module.css';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import config from './config.json';
+import {component} from './config.json';
+import CallRoundedIcon from '@mui/icons-material/Call';
+import { useColorPalates } from '../../molecules/theme-provider/hooks';
+import { Avatar } from '@mui/material';
 
 const FAQPage: React.FC = () => {
-  const downloadPDFHandler=()=>{
-    console.log(config.component.userManualText)
-  }
+  const theme = useColorPalates(); 
+  const downloadPDFHandler=useCallback(()=>{
+    console.log(component.userManualText ?? "User Manual")
+  },[])
 
-  const handleContactClick=()=>{
-    console.log(config.component.contactText)
-  }
+  const handleContactClick=useCallback(()=>{
+    console.log(component.contactText ??"Contact User")
+  },[])
 
   return (
     <>
@@ -20,24 +24,30 @@ const FAQPage: React.FC = () => {
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"></meta>
       <Box className={styles.main}>
-          <Box m={3}><Typography variant='h4' sx={{fontWeight:"600", color: config.theme.primaryColor.value}}>{config.component.title}</Typography></Box>
+          <Box m={3}><Typography variant='h4' sx={{fontWeight:"600", color: theme?.primary?.main}}>{component.title ?? "Faq"}</Typography></Box>
           <Box>
-            {config?.component?.userManualText && (
+            {component?.userManualText && (
               <Box className={styles.manualButtons} m={3}>
                 <Button
                   onClick={downloadPDFHandler}
-                  variant='contained' sx={{textTransform:'none', backgroundColor:config.theme.primaryColor.value, "&:hover":{backgroundColor:config.theme.primaryColor.value}}}>
-                  {config.component.userManualText}
+                  variant='contained' sx={{textTransform:'none', backgroundColor:theme?.primary?.main, "&:hover":{backgroundColor:theme?.primary?.main}}}>
+                  {component.userManualText ?? "User Manual"}
                 </Button>
               </Box>
             )}
-            {config?.component?.contactText && (
+            {component?.contactText && (
               <Box className={styles.dialerBox} m={3}>
-                <Box p={1.5}><Typography variant='body1' sx={{fontWeight:"bold"}}>{config.component.contactDescriptionText}</Typography>
+                <Box p={1.5}><Typography variant='body1' sx={{fontWeight:"bold"}}>{component.contactDescriptionText ?? "contact description"}</Typography>
                 </Box>
-                <Box px={2} >
-                  <img src={config.component.contactIcon} alt="callIcon" className={styles.callIconBox}/>
-                  <Button variant='text' size="large" onClick={handleContactClick} sx={{textTransform:'none',color:config.theme.primaryColor.value, "&:hover":{color:config.theme.primaryColor.value}}}><Typography variant='h5' fontWeight={600}>{config.component.contactText}</Typography></Button>
+                <Box px={2} display={'flex'} alignItems={"center"}>
+                  <Box><Avatar
+            sx={{ bgcolor: theme.primary.main, width:"5vh",height:"5vh" }}
+            alt="Call Icon"
+           >
+           <CallRoundedIcon fontSize='medium'/>
+           </Avatar></Box>
+                  <Button variant='text' size="large" onClick={handleContactClick} sx={{textTransform:'none',color:theme?.primary?.main, "&:hover":{color:theme?.primary?.main}}}>
+                    <Typography variant='h5' fontWeight={600}>{component.contactText ?? "Contact User"}</Typography></Button>
                 </Box>
               </Box>
             )}
