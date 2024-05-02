63d09c35540182ccf0469e2f3207e1bc0f6ce697
AMISH-KHAN <khanamish8923@gmail.com>
2024-05-03 01:25:13 +0530

    setting up decap cms

---
+++ public/admin/decap.js
@@ -0,0 +1,5 @@
+import CMS from "decap-cms-app";
+// Initialize the CMS object
+CMS.init();
+// Now the registry is available via the CMS object.
+CMS.registerPreviewTemplate("my-template", MyTemplate);
