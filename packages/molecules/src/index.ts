196f95f24ad955023972d112fec8b7f19dbc37fb
riya-2206 <riya@esmagico.in>
2024-07-25 16:02:15 +0530

    added new molecules for share buttons sidebar navbar

--- packages/molecules/src/index.ts
+++ packages/molecules/src/index.ts
@@ -4,10 +4,10 @@ import { FullPageLoader } from './fullpage-loader';
 import { ChatUI } from './chat-ui';
 import { JsonToTable } from './json-to-table';
 import MessageItem from './message-item';
-import ShareButtons from './share-buttons';
+import ShareButtons, { NewShareButtons } from './share-buttons';
 import VoiceRecorder from './voice-recorder';
 import Navbar from './navbar';
-import Sidebar from './sidebar';
+import Sidebar, { NewSidebar } from './sidebar';
 import LanguagePicker from './language-picker';
 import { NewLanguagePicker } from './language-picker';
 import BlinkingSpinner from './blinking-spinner';
@@ -21,7 +21,9 @@ export {
   MessageItem,
   Navbar,
   Sidebar,
+  NewSidebar,
   ShareButtons,
+  NewShareButtons,
   VoiceRecorder,
   OTPInput,
   LanguagePicker,
--- packages/molecules/src/navbar/index.tsx
+++ packages/molecules/src/navbar/index.tsx
@@ -9,6 +9,22 @@ import { Sidebar } from '../sidebar/index';
 // import ThemePicker from '../../components/theme-picker'
 import { useColorPalates, useUiConfig } from '@samagra-x/stencil-hooks';
 
+type Icon = {
+  id: string;
+  src: string;
+};
+
+type NavbarProps = {
+  brandName?: string;
+  onToggle: () => void;
+  isOpen: boolean;
+  showHamburgerMenu?: boolean;
+  leftHomeIcon?: Icon;
+  showHomeIcon?: boolean;
+  centerLogoIcons?: Icon[];
+  rightLogoIcons?: Icon[];
+};
+
 const Navbar: React.FC = () => {
   const config = useUiConfig('component', 'navbar');
 
@@ -105,3 +121,102 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+
+const NewNavbar: React.FC<NavbarProps> = ({
+  brandName,
+  onToggle,
+  isOpen,
+  showHamburgerMenu = true,
+  showHomeIcon = false,
+  leftHomeIcon = {},
+  centerLogoIcons = [],
+  rightLogoIcons = [],
+}) => {
+  const theme = useColorPalates();
+
+  return (
+    <>
+      <AppBar position="static" sx={{ background: theme.primary.dark }}>
+        <Toolbar style={{ display: 'flex-start', justifyContent: 'space-between' }}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            {showHamburgerMenu && (
+              <IconButton
+                size="large"
+                edge="start"
+                color="primary"
+                aria-label="open drawer"
+                sx={{ mr: 2 }}
+                onClick={onToggle}
+              >
+                <Menu />
+              </IconButton>
+            )}
+            {showHomeIcon && (
+              <div>
+                <IconButton
+                  color="primary"
+                  size="large"
+                  edge="start"
+                  aria-label="home"
+                  style={{ fontSize: '2rem', height: '48px' }}
+                >
+                  <Home />
+                </IconButton>
+                {leftHomeIcon && (
+                  <img
+                    src={leftHomeIcon.src}
+                    alt={`Left Home Icon ${leftHomeIcon.id}`}
+                    style={{ maxHeight: '48px' }}
+                  />
+                )}
+              </div>
+            )}
+          </div>
+
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              flex: 1,
+            }}
+          >
+            {centerLogoIcons &&
+              centerLogoIcons.map((logo: { id: string; src: string }) => (
+                <img
+                  key={logo.id}
+                  src={logo.src}
+                  alt={`Logo ${logo.id}`}
+                  style={{ maxHeight: '48px' }}
+                />
+              ))}
+
+            {brandName && (
+              <Typography variant="h6" color="inherit" sx={{ marginTop: 1, fontSize: '1.5rem' }}>
+                {brandName}
+              </Typography>
+            )}
+          </div>
+
+          {rightLogoIcons && (
+            <div>
+              {rightLogoIcons.map((logo: { id: string; src: string }) => (
+                <img
+                  key={logo.id}
+                  src={logo.src}
+                  alt={`Right Logo ${logo.id}`}
+                  style={{ maxHeight: '48px' }}
+                />
+              ))}
+            </div>
+          )}
+          {/* <ThemePicker /> */}
+        </Toolbar>
+      </AppBar>
+
+      <Sidebar isOpen={isOpen} onToggle={onToggle} />
+    </>
+  );
+};
+
+export { NewNavbar };
