628d6c66ee29cb2fc8fc7a77f9317ef2b82bc5b9
Ankit Patidar <ankit_ml12@kali.test>
2024-08-23 06:37:30 +0530

    fix: feedback page updated

--- apps/all-molecule-app/app/feedback-page/page.tsx
+++ apps/all-molecule-app/app/feedback-page/page.tsx
@@ -1,9 +1,83 @@
 'use client'
-import { FeedbackPage } from '@samagra-x/stencil-pages'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const FeebBack = () => {
-  return <FeedbackPage />
+import Feedback from '@samagra-x/stencil-molecules/lib/feedback'
+
+import { toast } from 'react-hot-toast'
+import { useColorPalates } from '../../provider/theme-provider/hooks'
+import { useConfig } from '../../provider/config-provider/hook'
+
+import axios from 'axios'
+// import Menu from '../../components/menu'
+
+const FeedbackPage: React.FC = () => {
+  const [star, setStar] = useState(1)
+  const [review, setReview] = useState('')
+  const theme = useColorPalates()
+  const config = useConfig('component', 'feedbackPage')
+
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.NEXT_PUBLIC_BFF_API_URL}/feedback/${localStorage.getItem('userID')}`,
+        {
+          headers: {
+            botId: process.env.NEXT_PUBLIC_BOT_ID || '',
+            orgId: process.env.NEXT_PUBLIC_ORG_ID || '',
+          },
+        }
+      )
+      .then((res) => {
+        setStar(res?.data?.rating)
+        setReview(res?.data?.review)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  const handleFeedback = () => {
+    if (!config) return
+
+    if (config?.ratingBox && star === 0) {
+      toast.error('Please provide a rating')
+      return
+    }
+
+    if (config?.reviewBox && review === '') {
+      toast.error('Please provide a review')
+      return
+    }
+
+    axios
+      .post(
+        `${process.env.NEXT_PUBLIC_BFF_API_URL}/feedback/${localStorage.getItem('userID')}`,
+        {
+          rating: star,
+          review: review,
+        },
+        {
+          headers: {
+            botId: process.env.NEXT_PUBLIC_BOT_ID || '',
+            orgId: process.env.NEXT_PUBLIC_ORG_ID || '',
+          },
+        }
+      )
+      .then(() => {
+        toast.success('Feedback submitted successfully')
+      })
+      .catch((error) => {
+        console.error('Error submitting feedback:', error)
+        toast.error('Failed to submit feedback. Please try again later.')
+      })
+  }
+
+  return (
+    <>
+      <Feedback star={5} showRatingBox={true} showReviewBox={true} />
+      {/* <Menu /> */}
+    </>
+  )
 }
 
-export default FeebBack
+export default FeedbackPage
