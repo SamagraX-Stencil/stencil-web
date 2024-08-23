72ca32aca3659591d5e12911cd18b6f978e6277d
Ankit Patidar <ankit_ml12@kali.test>
2024-08-23 06:53:34 +0530

    fix: comming soon page updated

--- apps/all-molecule-app/app/faq-page/page.tsx
+++ apps/all-molecule-app/app/faq-page/page.tsx
@@ -1,9 +1,138 @@
-'use client'
-import { FAQPage } from '@samagra-x/stencil-pages'
-import React from 'react'
+import React, { useCallback } from 'react'
+import styles from './index.module.css'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import CallRoundedIcon from '@mui/icons-material/Call'
+import { Avatar } from '@mui/material'
+import { useColorPalates } from '../../providers/theme-provider/hooks'
+import ComingSoonPage from '../coming-soon-page'
+import { useLocalization } from '../../hooks'
+import { useConfig } from '../../hooks/useConfig'
+import Menu from '../../components/menu'
 
-const Faq = () => {
-  return <FAQPage />
+const FAQPage: React.FC = () => {
+  const t = useLocalization()
+  const theme = useColorPalates()
+  const config = useConfig('component', 'faqPage')
+
+  const downloadPDFHandler = useCallback(() => {
+    const link: any = config?.faqManualPdfLink
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+
+    // window.open(link);
+
+    fetch(proxyUrl + link, {
+      method: 'GET',
+      headers: {},
+    })
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = window.URL.createObjectURL(new Blob([blob]))
+        const a = document.createElement('a')
+        a.style.display = 'none'
+        a.href = link
+        a.download = `User_Manual_For_VAWs.pdf`
+
+        document.body.appendChild(a)
+        a.click()
+
+        window.URL.revokeObjectURL(url)
+        document.body.removeChild(a)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [config?.faqManualPdfLink])
+
+  const handleContactClick = useCallback(() => {
+    const phoneNumber = `tel:${config?.faqPhoneNumber}`
+    window.location.href = phoneNumber
+  }, [config?.faqPhoneNumber])
+
+  if (!config?.showFaqPage) {
+    return <ComingSoonPage />
+  } else
+    return (
+      <>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        ></meta>
+        <Box className={styles.main}>
+          <Box m={3}>
+            <Typography
+              data-testid="faq-page-title"
+              variant="h4"
+              sx={{ fontWeight: '600', color: theme?.primary?.main }}
+            >
+              {t('label.faqs')}
+            </Typography>
+          </Box>
+          <Box>
+            {config?.faqShowPdfButton && (
+              <Box className={styles.manualButtons} m={3}>
+                <Button
+                  data-testid="faq-page-manual-button"
+                  onClick={downloadPDFHandler}
+                  variant="contained"
+                  sx={{
+                    textTransform: 'none',
+                    backgroundColor: theme?.primary?.main,
+                    '&:hover': { backgroundColor: theme?.primary?.main },
+                  }}
+                >
+                  {t('label.manual')}
+                </Button>
+              </Box>
+            )}
+            {config?.faqShowCallBox && (
+              <Box
+                className={styles.dialerBox}
+                m={3}
+                data-testid="faq-page-call-box"
+              >
+                <Box p={1.5}>
+                  <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+                    {t('message.dial_description')}
+                  </Typography>
+                </Box>
+                <Box px={2} display={'flex'} alignItems={'center'}>
+                  <Box>
+                    <Avatar
+                      sx={{
+                        bgcolor: theme.primary.main,
+                        width: '5vh',
+                        height: '5vh',
+                      }}
+                      alt="Call Icon"
+                    >
+                      <CallRoundedIcon fontSize="medium" />
+                    </Avatar>
+                  </Box>
+                  <Button
+                    variant="text"
+                    size="large"
+                    onClick={handleContactClick}
+                    sx={{
+                      textTransform: 'none',
+                      color: theme?.primary?.main,
+                      '&:hover': { color: theme?.primary?.main },
+                    }}
+                  >
+                    <Typography variant="h5" fontWeight={600}>{`${t(
+                      'label.dial'
+                    )} ${config?.faqPhoneNumber}`}</Typography>
+                  </Button>
+                </Box>
+              </Box>
+            )}
+          </Box>
+        </Box>
+
+        <Menu />
+      </>
+    )
 }
 
-export default Faq
+export default FAQPage
--- apps/all-molecule-app/app/history-page/page.tsx
+++ apps/all-molecule-app/app/history-page/page.tsx
@@ -1,9 +1,104 @@
-'use client'
-import { HistoryPage } from '@samagra-x/stencil-pages'
-import React from 'react'
+import { FC, useCallback, useEffect, useState } from 'react'
+import styles from './style.module.css'
 
-const History = () => {
-  return <HistoryPage />
+import { List } from '@samagra-x/stencil-molecules'
+import { IconButton } from '@mui/material'
+import { DeleteOutline, Forum } from '@mui/icons-material'
+import moment from 'moment'
+import { FullPageLoader } from '@samagra-x/stencil-molecules'
+import { ChatItem, HistoryItem } from './types'
+import { map } from 'lodash'
+import sample from './sample.json'
+import {
+  useUiConfig,
+  useColorPalates,
+  useLocalization,
+} from '@samagra-x/stencil-hooks'
+
+const HistoryPage: FC = () => {
+  const config = useUiConfig('component', 'historyPage')
+
+  const [isFetching, setIsFetching] = useState(true)
+  const [list, setList] = useState([])
+  const theme = useColorPalates()
+  const t = useLocalization()
+
+  const handleClick = useCallback((activeItem: HistoryItem) => {
+    console.log({ activeItem })
+  }, [])
+
+  const onSecondaryActionClick = useCallback(
+    (activeItem: ChatItem) => () => {
+      if (
+        window.confirm('Are you sure you want to delete this conversation?')
+      ) {
+        setList((prev) => {
+          return prev.filter(
+            (item: ChatItem) =>
+              item.conversationId !== activeItem.conversationId
+          )
+        })
+      }
+    },
+    []
+  )
+
+  useEffect(() => {
+    setIsFetching(true)
+    const historyList = map(sample, (chatItem: ChatItem) => ({
+      id: chatItem?.id,
+      label: chatItem?.query,
+      conversationId: chatItem?.conversationId,
+      userId: chatItem?.userId,
+      secondaryLabel: config.showTimestamp
+        ? moment(chatItem?.updatedAt).format('hh:mm A DD/MM/YYYY')
+        : '',
+      icon: <Forum style={{ color: theme?.primary?.light }} />,
+      secondaryAction: config.allowDelete && (
+        <IconButton
+          edge="end"
+          aria-label="comments"
+          onClick={onSecondaryActionClick(chatItem)}
+        >
+          <DeleteOutline />
+        </IconButton>
+      ),
+      onClick: handleClick,
+      isDivider: true,
+    }))
+    setTimeout(() => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      //@ts-expect-error
+      setList(historyList)
+      setIsFetching(false)
+    }, 2000)
+  }, [
+    handleClick,
+    onSecondaryActionClick,
+    config.showTimestamp,
+    config.allowDelete,
+    theme?.primary?.light,
+  ])
+
+  return (
+    <>
+      <div className={styles.main}>
+        <FullPageLoader loading={isFetching} color={theme?.primary?.main} />
+        <div className={styles.title} style={{ color: theme?.primary?.main }}>
+          {t('label.chats') ?? 'No Label Provided'}
+        </div>
+        <div className={styles.chatList}>
+          <List
+            items={list}
+            noItem={{
+              label: t('label.no_history') ?? 'No History Found',
+              icon: <Forum style={{ color: theme?.primary?.light }} />,
+            }}
+          />
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default History
+export default HistoryPage
